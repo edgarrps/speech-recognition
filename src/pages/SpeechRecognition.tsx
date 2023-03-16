23de2c30f39c1aeb5b364e93c0a821bd810bff66
Edgar Santos <edgarrps@gmail.com>
2023-03-16 17:11:55 -0400

    style improvements

--- src/pages/SpeechRecognition.tsx
+++ src/pages/SpeechRecognition.tsx
@@ -1,8 +1,11 @@
-export default function SpeechRecognition () {
-    return(
+import { useSpeechRecognition } from 'react-speech-recognition'
+
+export default function SpeechRecognition() {
+
+    return (
         <div className='grid place-items-center space-y-10 pt-20'>
-            <textarea className='rounded-md bg-gradient-to-r from-stone-200 via-purple-200 to-pink-300 w-[700px] h-[400px] resize-none pl-4 pt-4 pr-4 focus:outline-0'></textarea>
-            <button className='rounded-md bg-green-600 font-extrabold text-white pl-2 pr-2'>Clique</button>
+            <textarea className='rounded-md bg-sky-500/[.2] text-white font-mono font-semibold text-2xl w-[700px] h-[400px] resize-none pl-4 pt-4 pr-4 focus:outline-0'></textarea>
+            <button className='rounded-md bg-yellow-600/[.6] hover:bg-yellow-400/[.6] active:bg-yellow-500 font-extrabold text-white pl-2 pr-2'>Clique</button>
         </div>
     )
 }
