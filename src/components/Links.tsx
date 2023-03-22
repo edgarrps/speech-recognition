83976c38736a94bd0a332ea4df00c78628d96d59
edgarrps <edgarrps@gmail.com>
2023-03-21 23:56:50 -0400

    Props created on Links

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -4,15 +4,14 @@ import { Languages } from '../data/Languages'
 import { Language } from '../types/Language'
 import { mic } from '../utils/Recognition'
 
-export const Links = () => {
+type Props = {
+    lang: Language
+    map: any
+}
 
+export const Links = ({ lang }: Props) => {
+    mic.lang = lang.miclang
     const navigation = useNavigate()
-
-    const handleItemClick = (lang: Language) => {
-        mic.lang = lang.miclang
-        navigation(`/speech-recognition/${lang.miclang}`)
-    }
-
     const link = 'pl-2 pr-2 bg-green-500 rounded-md 1 hover:bg-green-400 hover:scale-110 ease-in-out duration-300'
 
     return (
@@ -27,9 +26,9 @@ export const Links = () => {
         inter: space-x-6
         tablet: space-x-4
         smartphone: space-x-2 fadein 10'>
-            {Languages.map((value, i) => (
+            {lang.map((value: string, i: number) => (
                 // <Link onClick={handleItemClick} key={i} to={`/${value.miclang}`} className={twMerge('link', link)}>{value.title}</Link>
-                <div onClick={handleItemClick} key={i} className={twMerge('link', link)}>{value.title}</div>
+                <div onClick={() => { navigation(`/speech-recognition/${lang.miclang}`) }} key={i} className={twMerge('link', link)}>{value.title}</div>
             ))}</div>
     )
 }
